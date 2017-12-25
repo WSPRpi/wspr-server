bc1871a960e74976e796271aef956a68242abb37
Michael Rawson <michael@rawsons.uk>
2017-12-25 12:31:14 +0000

    prevent zeroes from affecting UART transmission

--- wspr/wire_format.py
+++ wspr/wire_format.py
@@ -2,7 +2,7 @@ def for_wire(command, rest):
 	return '{}{};\n'.format(command, rest).encode('ascii')
 
 def from_wire(data):
-	data = data.decode('ascii')
+	data = data.decode('ascii').replace('\x00', '')
 	command = data[0]
 	rest = data[1:-2]
 	return (command, rest)
