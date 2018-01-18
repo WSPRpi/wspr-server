1c80c7c9113e9d07bcd773e1628396acc8a0bb97
Michael Rawson <michael@rawsons.uk>
2018-01-18 10:33:21 +0000

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="wspr-server",
-	version="0.1.4",
+	version="0.1.5",
 	url="https://github.com/wsprpi/wspr-software",
 	author="Michael Rawson",
 	author_email="michael@rawsons.uk",
