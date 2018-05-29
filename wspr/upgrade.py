f866252568b2e933bf8e1906ca42815e47e27c9a
Michael Rawson <michael@rawsons.uk>
2018-05-29 08:33:51 +0100

    catch programs that are not installed

--- wspr/upgrade.py
+++ wspr/upgrade.py
@@ -6,9 +6,11 @@ from requests import get as download
 
 def execute(log, *args):
     try:
+        raise FileNotFoundError("test")
         return Popen(args, stdout=PIPE)
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         log("error: {} not found".format(args[0]))
+        log("{}".format(e))
 
 
 def software_upgrade(log):
